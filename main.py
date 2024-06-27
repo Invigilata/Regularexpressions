9f014e65bb3dd37347b68280cfc9d9285b94ff2f
Invigilata <vernitemnesteam2018@mail.ru>
2024-06-27 14:57:29 +0300

    1

---
+++ main.py
@@ -0,0 +1,20 @@
+import re
+
+
+def extract_image_links(html_text):
+    pattern = r'<img\s+[^>]*src=["\'](https?://[^"\']+\.(?:jpg|jpeg|png|gif))["\']'
+
+    image_links = re.findall(pattern, html_text, re.IGNORECASE)
+
+    return image_links
+
+
+# Пример
+sample_html = "<img src='https://example.com/image1.jpg'> <img src='http://example.com/image2.png'> <img src='https://example.com/image3.gif'>"
+
+image_links = extract_image_links(sample_html)
+if image_links:
+    for image_link in image_links:
+        print(image_link)
+else:
+    print("Нет ссылок с картинками в HTML тексте.")
